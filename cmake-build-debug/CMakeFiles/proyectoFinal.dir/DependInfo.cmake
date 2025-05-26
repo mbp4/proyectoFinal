1a8f7ef4087eb399dbee3797e96f66b90035b6dc
Sira <sgonzmad@myuax.com>
2025-05-26 14:53:30 +0200

    commit

---
+++ cmake-build-debug/CMakeFiles/proyectoFinal.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/siragonzalez-madrono/Desktop/proyectoFinal/difusion_secuencial.c" "CMakeFiles/proyectoFinal.dir/difusion_secuencial.c.o" "gcc" "CMakeFiles/proyectoFinal.dir/difusion_secuencial.c.o.d"
+  "/Users/siragonzalez-madrono/Desktop/proyectoFinal/main.c" "CMakeFiles/proyectoFinal.dir/main.c.o" "gcc" "CMakeFiles/proyectoFinal.dir/main.c.o.d"
+  "/Users/siragonzalez-madrono/Desktop/proyectoFinal/utilidades.c" "CMakeFiles/proyectoFinal.dir/utilidades.c.o" "gcc" "CMakeFiles/proyectoFinal.dir/utilidades.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/proyectoFinal.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proyectoFinal.dir/difusion_secuencial.c.o"
+  "CMakeFiles/proyectoFinal.dir/difusion_secuencial.c.o.d"
+  "CMakeFiles/proyectoFinal.dir/main.c.o"
+  "CMakeFiles/proyectoFinal.dir/main.c.o.d"
+  "CMakeFiles/proyectoFinal.dir/utilidades.c.o"
+  "CMakeFiles/proyectoFinal.dir/utilidades.c.o.d"
+  "proyectoFinal"
+  "proyectoFinal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proyectoFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

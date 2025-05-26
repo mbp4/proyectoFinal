e9a26f722b6b1be5811437daebe0b7085de5b1fd
Sira <sgonzmad@myuax.com>
2025-05-26 14:55:53 +0200

    prueba

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+// Declaración de la función secuencial
+void difusion_secuencial(int N, float umbral, int max_iter);
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        printf("Uso: %s <N> <umbral> <max_iter>\n", argv[0]);
+        return 1;
+    }
+
+    int N = atoi(argv[1]);
+    float umbral = atof(argv[2]);
+    int max_iter = atoi(argv[3]);
+
+    difusion_secuencial(N, umbral, max_iter);
+
     return 0;
 }

b78a62c024b868f1fff734fb130b0e51e8bb17ad
mbp4 <mblanpon@myuax.com>
2025-05-26 19:27:10 +0200

    comentarios

--- difusion_paralela.c
+++ difusion_paralela.c
@@ -5,23 +5,26 @@
 #include <stdio.h>
 #include <math.h>
 #include <omp.h>
-#include "utilidades.h"
+#include "utilidades.h" //importamos nuestras funciones para poder crear, inicializar, liberar y usar la matriz
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 
 void difusion_paralela(int N, float umbral, int max_iter) {
     float** actual = crear_matriz(N);
     float** siguiente = crear_matriz(N);
+    //creamos dos matrices una para guardar los valores actuales y otra para los siguientes valores obtenidos
     inicializar_matriz(actual, N);
+    //inicializamos la matriz con los valores pedidos en el enunciado
 
-    int iter = 0;
-    float cambio_max;
-    double inicio = omp_get_wtime();
+    int iter = 0; //contador de iteraciones
+    float cambio_max; //guarda el máximo cambio de temperatura entre iteracciones
+    double inicio = omp_get_wtime(); //función de OpenMP para medir el tiempo que tarda la simulación
 
     do {
-        cambio_max = 0.0;
+        cambio_max = 0.0; //este se reiniciará cada vez que se llegué al máximo de iteraciones
 
 
+        //bucle paralelo para actualizar los valores de "siguiente" y calcular el cambio de temperatura haciendo uso de reduction
 #pragma omp parallel for reduction(max:cambio_max) collapse(2)
         for (int i = 1; i < N - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
@@ -31,6 +34,7 @@ void difusion_paralela(int N, float umbral, int max_iter) {
             }
         }
 
+        //copia los valores calculados de "siguiente" a la "actual" para las proximas iteraciones
 #pragma omp parallel for collapse(2)
         for (int i = 1; i < N - 1; i++) {
             for (int j = 1; j < N - 1; j++) {
@@ -41,9 +45,10 @@ void difusion_paralela(int N, float umbral, int max_iter) {
         iter++;
     } while (cambio_max > umbral && iter < max_iter);
 
-    double fin = omp_get_wtime();
-    printf("Paralela: iteraciones = %d, tiempo = %.4f s\n", iter, fin - inicio);
+    double fin = omp_get_wtime(); //obtenemos el tiempo final
+    printf("Paralela: iteraciones = %d, tiempo = %.4f s\n", iter, fin - inicio); //mostramos cuantas iteraciones se realizaron y el tiempo total
 
+    //liberamos la memoria reservada para ambas matrices
     liberar_matriz(actual, N);
     liberar_matriz(siguiente, N);
 }
